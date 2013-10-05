8cd854d879ba9e4d82a7eaa8462958114e1577da
Dmitry Zakablukov <dmitriy.zakablukov@gmail.com>
2013-10-05 17:01:51 +0400

    GUI: show test window by menu command

--- WorkRecorder/WorkRecorder/Package/RecorderPackage.cs
+++ WorkRecorder/WorkRecorder/Package/RecorderPackage.cs
@@ -1,23 +1,7 @@
-﻿//using System;
-////using System.Diagnostics;
-////using System.Globalization;
-//using System.Runtime.InteropServices;
-//using System.ComponentModel.Design;
-////using Microsoft.Win32;
-////using Microsoft.VisualStudio;
-//using Microsoft.VisualStudio.Shell.Interop;
-////using Microsoft.VisualStudio.OLE.Interop;
-//using Microsoft.VisualStudio.Shell;
-
-using System;
-using System.Diagnostics;
-using System.Globalization;
+﻿using System;
 using System.Runtime.InteropServices;
 using System.ComponentModel.Design;
-using Microsoft.Win32;
-using Microsoft.VisualStudio;
 using Microsoft.VisualStudio.Shell.Interop;
-using Microsoft.VisualStudio.OLE.Interop;
 using Microsoft.VisualStudio.Shell;
 
 namespace Community.WorkRecorder.VsPackage
@@ -31,7 +15,7 @@ namespace Community.WorkRecorder.VsPackage
     [PackageRegistration(UseManagedResourcesOnly = true)]
     // This attribute is used to register the informations needed to show the this package
     // in the Help/About dialog of Visual Studio.
-    [InstalledProductRegistration("#110", "#111", "1.0", IconResourceID = 400)]
+    [InstalledProductRegistration("#110", "#111", "1.0" /*Resources.PluginVersion*/, IconResourceID = 400)]
     // This attribute is needed to let the shell know that this package exposes some menus.
     [ProvideMenuResource("Menus.ctmenu", 1)]
     // This attribute registers a tool window exposed by this package.
@@ -41,20 +25,6 @@ namespace Community.WorkRecorder.VsPackage
     {
         public RecorderPackage()
         {
-        }
-
-        private void ShowWorkRecorderWindow(object sender, EventArgs e)
-        {
-            // Get the instance number 0 of this tool window. This window is single instance so this instance
-            // is actually the only one.
-            // The last flag is set to true so that if the tool window does not exists it will be created.
-            ToolWindowPane window = this.FindToolWindow(typeof(GUI.RecorderWindow), 0, true);
-            if ((null == window) || (null == window.Frame))
-            {
-                throw new NotSupportedException(Resources.CanNotCreateWindow);
-            }
-            IVsWindowFrame windowFrame = (IVsWindowFrame)window.Frame;
-            Microsoft.VisualStudio.ErrorHandler.ThrowOnFailure(windowFrame.Show());
         }
 
         /////////////////////////////////////////////////////////////////////////////
@@ -83,5 +53,18 @@ namespace Community.WorkRecorder.VsPackage
         // Override Package Implementation
         /////////////////////////////////////////////////////////////////////////////
 
+        private void ShowWorkRecorderWindow(object sender, EventArgs e)
+        {
+            // Get the instance number 0 of this tool window. This window is single instance so this instance
+            // is actually the only one.
+            // The last flag is set to true so that if the tool window does not exists it will be created.
+            ToolWindowPane window = this.FindToolWindow(typeof(GUI.RecorderWindow), 0, true);
+            if ((null == window) || (null == window.Frame))
+            {
+                throw new NotSupportedException(Resources.CanNotCreateWindow);
+            }
+            IVsWindowFrame windowFrame = (IVsWindowFrame)window.Frame;
+            Microsoft.VisualStudio.ErrorHandler.ThrowOnFailure(windowFrame.Show());
+        }
     }
 }
